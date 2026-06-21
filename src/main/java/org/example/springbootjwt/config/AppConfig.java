53956ccd561e9747c9c6e103239a7d20b316f586
Gökhan Zopcuk <gzopcuk1@gmail.com>
2026-06-22 01:11:28 +0300

    appconfig %% securityConfig

---
+++ src/main/java/org/example/springbootjwt/config/AppConfig.java
@@ -0,0 +1,58 @@
+package org.example.springbootjwt.config;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.example.springbootjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.*;
+import org.example.springbootjwt.model.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+@Configuration
+public class AppConfig {
+    //“Spring Security’ye diyoruz ki: kullanıcı login olmaya çalıştığında git veritabanından bul, şifresini kontrol et, doğruysa sisteme al.”
+
+    @Autowired
+    private org.example.springbootjwt.repository.UserRepository userRepository;
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                Optional<User> optional= userRepository.findByUsername(username);
+                if (optional.isPresent()){
+                    return  optional.get();
+                }
+                return null;
+
+            }
+        };
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider =
+                new DaoAuthenticationProvider(userDetailsService());
+
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration){
+        return  configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/org/example/springbootjwt/config/securityConfig.java
@@ -0,0 +1,47 @@
+package org.example.springbootjwt.config;
+
+import org.example.springbootjwt.jwt.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+//Bu sınıf JWT projesinin güvenlik kapısıdır. Burada Spring Security'ye hangi isteklere izin vereceğimizi ve JWT kontrolünün nasıl yapılacağını söylüyoruz.
+@Configuration
+@EnableWebSecurity
+public class securityConfig {
+    private  static  final  String AUTHENTICATE="/authenticate";
+    private  static  final  String REGISTER="/register";
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+@Autowired
+      private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+       http.authorizeHttpRequests(request-> request.requestMatchers(AUTHENTICATE,REGISTER)
+               .permitAll().anyRequest().authenticated())
+               .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .authenticationProvider(authenticationProvider)
+               .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+
+       return  http.build();
+   }
+
+
+}/*
+        Kısaca bu sınıfın yaptığı 4 şey:
+
+        /authenticate ve /register herkese açık
+        Diğer bütün endpointler JWT ister
+        Session kullanımını kapatır (STATELESS)
+        JwtAuthenticationFilter'ı Spring Security zincirine ekler.*/
